0b7d7b6591ec6f59372e98dd1d173093479358aa
sfc-gh-ccardillo <ccardillo@snowflake.com>
2024-01-30 11:05:34 -0500

    jupyter does not need token

--- dockerfiles/ollama/startup.sh
+++ dockerfiles/ollama/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start Jupyter Notebook
-jupyter notebook --ip='*' --port=8888 --no-browser --allow-root
+jupyter notebook --ip='*' --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
 
 #for running service as container starts
 #I do this manually in the example
